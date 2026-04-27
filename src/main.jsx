bebdcdbdc1e4e486e474898b36c4466493dd1c9b
Doni Wahyudi <doni.wahyudi@btpnsyariah.com>
2026-04-27 18:38:21 +0700

    Fix basename and asset routing issues

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/web_law">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
